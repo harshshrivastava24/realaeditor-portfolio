4744e2770f473c0abdd81664047c285c79311338
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2026-06-16 21:42:03 +0530

    optimization

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,11 +26,11 @@ const Navbar = () => {
 
         
           <ul className={`${styles.mobileList} ${isOpen ? styles.open : ''}`}>
-          <li><a href="#projects" className={styles.links}>Projects</a></li>
-          <li><a href="#services">Services</a></li>
-          {/* <li>Process</li> */}
-          <li><a href="#about">About</a></li>
-          <li><a href='#book' className={styles.mobileBook}>Book Call</a></li>
+          <li><a href="#projects" className={styles.links} onClick={toggleMenu}>Projects</a></li>
+          <li><a href="#services" onClick={toggleMenu}>Services</a></li>
+          {/* <li onClick={toggleMenu}>Process</li> */}
+          <li><a href="#about" onClick={toggleMenu}>About</a></li>
+          <li><a href='#book' onClick={toggleMenu} className={styles.mobileBook}>Book Call</a></li>
         </ul>
         
 
